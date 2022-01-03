ec5ec6cb59d549ecce60641f6c8826da8fdd4dcd
antonia <maryantoniapereira@gmail.com>
2022-01-03 17:50:56 -0200

    Criando o CRUD de pedido

--- src/main/java/ifrn/projeto/finolanche/controllers/PratoController.java
+++ src/main/java/ifrn/projeto/finolanche/controllers/PratoController.java
@@ -93,4 +93,86 @@ public class PratoController {
 
 	}
 	
+	@GetMapping("/{id}/selecionar")
+	public ModelAndView selecionarPrato(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView();
+		Optional<Prato> opt = pr.findById(id);
+		if (opt.isEmpty()) {
+			mv.setViewName("redirect:/site");
+			return mv;
+		}
+
+		Prato prato = opt.get();
+		mv.setViewName("site/formPrato");
+		mv.addObject("prato", prato);
+		return mv;
+	}
+	
+	@PostMapping("/{idPrato}")
+	public ModelAndView salvarPedido(@PathVariable Long idPrato, @Valid Pedido pedido,
+			BindingResult result, RedirectAttributes atributos) {
+
+		if (result.hasErrors()) {
+			atributos.addFlashAttribute("mensagem", "Verifique os campos!");
+			return detalharPrato(idPrato, pedido);
+		}
+
+		Optional<Prato> opt = pr.findById(idPrato);
+		ModelAndView mv = new ModelAndView();
+
+		
+
+		Prato prato = opt.get();
+		pedido.setPrato(prato);
+
+		rp.save(pedido);
+		atributos.addFlashAttribute("mensagem", "Pedido realizado com sucesso!");
+
+		mv.setViewName("redirect:/site/{idPrato}");
+		return mv;
+	}
+	
+	@GetMapping("/{idPrato}/pedidos/{idPedido}/selecionar")
+	public ModelAndView selecionarPedido(@PathVariable Long idPrato, @PathVariable Long idPedido) {
+		ModelAndView mv = new ModelAndView();
+
+		Optional<Prato> optPrato = pr.findById(idPrato);
+		Optional<Pedido> optPedido = rp.findById(idPedido);
+
+		if (optPrato.isEmpty() || optPedido.isEmpty()) {
+			mv.setViewName("redirect:/site");
+			return mv;
+		}
+
+		Prato prato = optPrato.get();
+		Pedido pedido = optPedido.get();
+
+		if (prato.getId() != pedido.getPrato().getId()) {
+			mv.setViewName("redirect:/site");
+			return mv;
+
+		}
+
+		mv.setViewName("site/detalhes");
+		mv.addObject("pedido", pedido);
+		mv.addObject("prato", prato);
+		mv.addObject("pedidos", rp.findByPrato(prato));
+
+		return mv;
+	}
+	
+	@GetMapping("/{idPrato}/pedidos/{id}/remover")
+	public String apagarPedido(@PathVariable Long id, RedirectAttributes attributes) {
+		Optional<Pedido> opt = rp.findById(id);
+		if (!opt.isEmpty()) {
+			rp.deleteById(id);
+
+			attributes.addFlashAttribute("mensagem", "Pedido removida com sucesso!");
+
+		}
+
+		return "redirect:/site/{idPrato}";
+
+	}
+	
 }
