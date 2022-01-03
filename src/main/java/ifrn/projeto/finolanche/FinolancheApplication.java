2451b9dadd4326f00c116fc686c2be50ba8fc59a
antonia <maryantoniapereira@gmail.com>
2022-01-03 16:55:26 -0200

    Iniciando o projeto

---
+++ src/main/java/ifrn/projeto/finolanche/FinolancheApplication.java
@@ -0,0 +1,13 @@
+package ifrn.projeto.finolanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinolancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinolancheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifrn/projeto/finolanche/FinolancheApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.projeto.finolanche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinolancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
