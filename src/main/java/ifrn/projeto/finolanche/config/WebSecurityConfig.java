b9889256f8f016681346cf830dd879353ffd2806
antonia <maryantoniapereira@gmail.com>
2022-01-04 10:13:34 -0200

    Últimas correções

--- src/main/java/ifrn/projeto/finolanche/config/WebSecurityConfig.java
+++ src/main/java/ifrn/projeto/finolanche/config/WebSecurityConfig.java
@@ -6,9 +6,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	
+	@Override
 	protected void configure (HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").permitAll()
+		http.authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("site/formPrato").hasRole("USU_LANCHONETE")
+		.antMatchers("site/listarPratos").hasRole("USU_CADASTRADO")
+		.antMatchers("site/detalhes").hasRole("USU_CADASTRADO")
 		.anyRequest().authenticated().and().formLogin().loginPage("/login")
 		.permitAll().and().logout().logoutSuccessUrl("/login?logout").permitAll();
 		
